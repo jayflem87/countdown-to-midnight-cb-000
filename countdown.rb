e3bf3eb175b4fd57180a853ec06b005351996ad3
Jay Fleming <jacobmfleming@gmail.com>
2018-02-15 18:06:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ def countdown(number)
 
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+end
